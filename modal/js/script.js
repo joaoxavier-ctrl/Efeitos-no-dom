e8386a022ac84ec78ccfc1b547fcbbe6118180de
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-25 17:23:07 -0300

    modal

---
+++ modal/js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
