9a395acc142810f8d38e5fd2b42d3190bbbce1f5
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-26 18:32:53 -0300

    outside-click

--- dropdown-menu/js/modules/dropdown-menu.js
+++ dropdown-menu/js/modules/dropdown-menu.js
@@ -1,15 +1,19 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu(){
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-}
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        });
+    });
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    menu.addEventListener('touchstart', handleClick);
-    menu.addEventListener('click', handleClick);
-})
-
-function handleClick(event){
-    event.preventDefault();
-    this.classList.toggle('active');
+    function handleClick(event){
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active');
+        });
+    };
 }
